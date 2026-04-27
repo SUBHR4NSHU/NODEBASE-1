79e3d927a6f96da3159b5f2d0bd77d75166ac143
SUBHR4NSHU <subhranshumohanty127@gmail.com>
2026-04-27 18:43:08 +0530

    feat: integrate Sentry for error tracking and performance monitoring across client, server, and edge environments

---
+++ sentry.server.config.ts
@@ -0,0 +1,28 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://61e93ee016aa11cb0298bf3952678c5d@o4511292019703808.ingest.us.sentry.io/4511292034842624",
+
+  integrations: [
+    // Add the Vercel AI SDK integration to sentry.server.config.ts
+    Sentry.vercelAIIntegration({
+      recordInputs: true,
+      recordOutputs: true,
+    }),
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  sendDefaultPii: true,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
