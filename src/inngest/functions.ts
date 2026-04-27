082611ab61c384ed72f2a0d492dd6debbf22d6fa
SUBHR4NSHU <subhranshumohanty127@gmail.com>
2026-04-27 17:58:40 +0530

    feat: integrate Vercel AI SDK with Inngest to orchestrate multi-model LLM workflows via tRPC

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,53 @@
-// src/inngest/functions.ts
+import prisma from "@/lib/db";
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
+import { createOpenAI } from '@ai-sdk/openai';
+import { createAnthropic } from '@ai-sdk/anthropic';
+import { generateText } from 'ai';
 
-export const processTask = inngest.createFunction(
-  { id: "process-task", triggers: { event: "app/task.created" } },
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+const anthropic = createAnthropic();
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute/ai" },
   async ({ event, step }) => {
-    const result = await step.run("handle-task", async () => {
-      return { processed: true, id: event.data.id };
+    await step.sleep('pretend', '5s');
+
+    const { steps: geminiSteps } = await step.ai.wrap('gemini-generate-text',
+      generateText, {
+        model: google('gemini-2.5-flash'),
+        system: 'You are a helpful assistant.',
+        prompt: 'what is 2 + 2?',
+      }
+    );
+    const { steps: openaiSteps } = await step.ai.wrap('openai-generate-text',
+      generateText, {
+        model: openai('gpt-4'),
+        system: 'You are a helpful assistant.',
+        prompt: 'what is 2 + 2?',
+      }
+    );
+    const { steps: anthropicSteps } = await step.ai.wrap('anthropic-generate-text',
+      generateText, {
+        model: anthropic('claude-opus-4-0'),
+        system: 'You are a helpful assistant.',
+        prompt: 'what is 2 + 2?',
+      }
+    );
+    await step.run('create-workflow', () => {
+      return prisma.workflow.create({
+        data: {
+          name: 'workflow-from-inngest',
+        }
+      });
     });
 
-    await step.sleep("pause", "1s");
-
-    return { message: `Task ${event.data.id} complete`, result };
-  }
+    return  {
+      geminiSteps,
+      openaiSteps,
+      anthropicSteps,
+    };
+  },
 );
